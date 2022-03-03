dfd660365d7fc2d837e87dcd7e98db8a0314d879
AIda <aida2001@mail.ru>
2022-03-03 08:16:20 +0000

    file

---
+++ user/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Post
+from .forms import UserRegistrationForm
+
+# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Регистрация прошла успешно!")
+            return redirect("main_tape")
+        else:
+            messages.error(request, "Ошибка при регистрации!")
+    else:
+        form = UserRegistrationForm()
+    context = {
+        "form" : form,
+    }
+    return render(request, "user/registration.html", context=context)
