1515b1e75a1dedd787eae2eceeecc16543576392
Костиков Егор <kostikovev@mail.ru>
2022-03-09 17:22:18 +0300

    Login bg

--- clonestagram/clonestagram/settings.py
+++ clonestagram/clonestagram/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 
 STATIC_ROOT = DB_CONFIG.get('projects', {}).get('clonestagram', {}).get('static_dir', 'clonestagram_static')
 
