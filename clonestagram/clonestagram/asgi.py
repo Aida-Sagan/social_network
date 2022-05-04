d347eb005b7545e1135cafc44722c4ece15d646b
Egor Kostikov <kostikovev@mail.ru>
2022-05-04 15:57:26 +0300

    test

--- clonestagram/clonestagram/asgi.py
+++ clonestagram/clonestagram/asgi.py
@@ -14,6 +14,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import messenger.routing
 
+
+get_asgi_application()
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clonestagram.settings")
 
 application = ProtocolTypeRouter({
